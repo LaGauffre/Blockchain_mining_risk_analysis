baef88ba69592e1ad90ece2f5952152e51d7625c
Pierre-O Goffard <pierre.olivier.goffard@gmail.com>
2021-01-07 08:24:27 +0100

    kWh in plots

--- Python/revenue_selfish_eyal_sirer.py
+++ Python/revenue_selfish_eyal_sirer.py
@@ -62,20 +62,20 @@ def U_N_t_ES(n, p, q, Z_0, rg):
         #Initialization of the buffer
         # if the buffer is empty
         if Z_ES[-1] == 0:
-            U_ES.append(0)
+            U_ES.append(U_ES[-1] + 0)
             Z_ES.append(Z_ES[-1] + ξ[k])
         # If the buffer is empty and a fork is ongoing
         elif Z_ES[-1] == 0.5:
             Z_ES.append(0)
             if ξ[k] == 1:
-                U_ES.append(2)
+                U_ES.append(U_ES[-1] + 2)
             elif ξ[k] == 0 and ζ[k] == 1:
-                U_ES.append(1)
+                U_ES.append(U_ES[-1] + 1)
             elif ξ[k] == 0 and ζ[k] == 0:
-                U_ES.append(0)
+                U_ES.append(U_ES[-1] + 0)
         # If the buffer contains 1 block
         elif Z_ES[-1] == 1:
-            U_ES.append(0)
+            U_ES.append(U_ES[-1] + 0)
             #If Sam finds a block
             if ξ[k] == 1:
                 Z_ES.append(Z_ES[-1] + ξ[k])
@@ -85,11 +85,11 @@ def U_N_t_ES(n, p, q, Z_0, rg):
         elif Z_ES[-1] == 2:
             #If Sam finds a block
             if ξ[k] == 1:
-                U_ES.append(0)
+                U_ES.append(U_ES[-1] + 0)
                 Z_ES.append(Z_ES[-1] + ξ[k])
             # If not then he releases all the block and get rewarded
             else:
-                U_ES.append(
+                U_ES.append( U_ES[-1] + 
                     sum(
                         np.ediff1d(
                             np.array(Z_ES[(np.where(
@@ -97,7 +97,7 @@ def U_N_t_ES(n, p, q, Z_0, rg):
                 Z_ES.append(0)
         # If the buffer contains more than 2 blocks
         elif Z_ES[-1] > 2:
-            U_ES.append(0)
+            U_ES.append(U_ES[-1] + 0)
             #If Sam finds a block
             if ξ[k] == 1:
                 Z_ES.append(Z_ES[-1] + ξ[k])
@@ -106,37 +106,37 @@ def U_N_t_ES(n, p, q, Z_0, rg):
                 Z_ES.append(Z_ES[-1] + (ξ[k]-1))
         ############################
         if Z_AG[-1] == 0:
-            U_AG.append(0)
+            U_AG.append(U_AG[-1] + 0)
             Z_AG.append(Z_AG[-1] + ξ[k])
         # If the buffer is empty and a fork is ongoing
         elif Z_AG[-1] == 0.5:
             Z_AG.append(0)
             if ξ[k] == 1:
-                U_AG.append(2)
+                U_AG.append(U_AG[-1] + 2)
             elif ξ[k] == 0 and ζ[k] == 1:
-                U_AG.append(1)
+                U_AG.append(U_AG[-1] + 1)
             elif ξ[k] == 0 and ζ[k] == 0:
-                U_AG.append(0)
+                U_AG.append(U_AG[-1] + 0)
         # If the buffer contains 1 block
         elif Z_AG[-1] == 1:
             #If Sam finds a block
             if ξ[k] == 1:
                 Z_AG.append(0)
-                U_AG.append(2)
+                U_AG.append(U_AG[-1] + 2)
             else:
                 Z_AG.append(0.5)
-                U_AG.append(0)
+                U_AG.append(U_AG[-1] + 0)
         
-    U_ES.append(0)
-    U_AG.append(0)
-    
+    U_ES.append(U_ES[-1] + 0)
+    U_AG.append(U_AG[-1] + 0)
+
     return({'U_ES':np.array(U_ES),'U_AG':np.array(U_AG)})
 
 
 
-# Test
-n, p, q, Z_0, rg = 10, 0.3, 0.5, 0, Generator(PCG64(14))
-print(U_N_t_ES(n, p, q, Z_0, rg))
+# # Test
+# n, p, q, Z_0, rg = 20, 0.4, 0.5, 0, Generator(PCG64(14))
+# print(U_N_t_ES(n, p, q, Z_0, rg))
 
 
 # Function that produce simulated data for exponential time horizon
@@ -154,18 +154,22 @@ def V_T_self_MC_ES(u, t, λ, p, q, b, c, sim_data):
     T_exp = sim_data['T_exp']
     K = len(T_exp)
     # Number of jumps up to time t
-    N_t = sim_data['N_t']
+    # N_t = sim_data['N_t']
     # Jump times when R_t > 0
     T = sim_data['T'] 
     # Reward associated to each jump
     U = sim_data['U']
 
     # Surplus at the end of the time horizon
-    R_t_AG = np.array([u - c * T_exp[k] + b * sum(U[k]['U_AG']) for k in range(K)])
-    R_t_ES = np.array([u - c * T_exp[k] + b * sum(U[k]['U_ES']) for k in range(K)])
+    # R_t_AG = np.array([u - c * T_exp[k] + b * sum(U[k]['U_AG']) for k in range(K)])
+    # R_t_ES = np.array([u - c * T_exp[k] + b * sum(U[k]['U_ES']) for k in range(K)])
+    R_t_AG = np.array([u - c * T_exp[k] + b * U[k]['U_AG'][-1] for k in range(K)])
+    R_t_ES = np.array([u - c * T_exp[k] + b * U[k]['U_ES'][-1] for k in range(K)])
     # Surplus at check times
-    R_s_AG = [np.array([u - c * T[k][i] + b * sum(U[k]['U_AG'][0:i]) for i in range(N_t[k] + 2)]) for k in range(K)]
-    R_s_ES = [np.array([u - c * T[k][i] + b * sum(U[k]['U_ES'][0:i]) for i in range(N_t[k] + 2)]) for k in range(K)]
+    # R_s_AG = [np.array([u - c * T[k][i] + b * sum(U[k]['U_AG'][0:i]) for i in range(N_t[k] + 2)]) for k in range(K)]
+    # R_s_ES = [np.array([u - c * T[k][i] + b * sum(U[k]['U_ES'][0:i]) for i in range(N_t[k] + 2)]) for k in range(K)]
+    R_s_AG = [u - c * T[k] + b * np.insert(U[k]['U_AG'][:-1],0,0) for k in range(K)]
+    R_s_ES = [u - c * T[k] + b * np.insert(U[k]['U_ES'][:-1],0,0) for k in range(K)]
     no_ruin_AG = np.array([np.all(R_s_AG[k] > 0) for k in range(K)])
     no_ruin_ES = np.array([np.all(R_s_ES[k] > 0) for k in range(K)])
     ψ_AG = 1 - np.mean(no_ruin_AG)
@@ -177,8 +181,8 @@ def V_T_self_MC_ES(u, t, λ, p, q, b, c, sim_data):
 
 # Test
 # Parameter operational cost
-# pkW, network_yearly_tW, pBTC, nBTC = 0.04, 77.78, 9938, 12.5
-# u, t, λ, p, q,Z_0, b = 25000, 12, 6, 0.02, 0.1,0, pBTC * nBTC
-# rg = Generator(PCG64(123))
-# c, sim_data = p * pkW * network_yearly_tW*10**9 / 365 / 24, sim_data_self_T_ES(t, λ, p, q, Z_0, 10000, rg)
-# V_T_self_MC_ES(u, t, λ, p, q, b, c, sim_data)
+pkW, network_yearly_tW, pBTC, nBTC = 0.04, 77.78, 9938, 12.5
+u, t, λ, p, q,Z_0, b = 250000, 12, 6, 0.2, 0.1,0, pBTC * nBTC
+rg = Generator(PCG64(123))
+c, sim_data = p * pkW * network_yearly_tW*10**9 / 365 / 24, sim_data_self_T_ES(t, λ, p, q, Z_0, 20000, rg)
+V_T_self_MC_ES(u, t, λ, p, q, b, c, sim_data)
